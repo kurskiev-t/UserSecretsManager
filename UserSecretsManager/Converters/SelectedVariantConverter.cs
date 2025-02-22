044c9672990d0966865235b393970cba765e50b8
Timur Kurskiev <kurskievtr@gmail.com>
2025-02-22 18:39:49 +0300

    упростил привязывание в IsChecked radiobutton

--- UserSecretsManager/Converters/SelectedVariantConverter.cs
+++ UserSecretsManager/Converters/SelectedVariantConverter.cs
@@ -6,27 +6,6 @@ using UserSecretsManager.Models;
 
 namespace UserSecretsManager.Converters
 {
-    //public class SelectedVariantConverter : IMultiValueConverter
-    //{
-    //    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-    //    {
-    //        if (values.Length != 2 || values[0] is not SecretSectionModel currentlySelectedSectionInUi || values[1] is not SecretSectionModel selectedSectionVariantInGroup)
-    //        {
-    //            return false;
-    //        }
-    //        return currentlySelectedSectionInUi == selectedSectionVariantInGroup;
-    //    }
-
-    //    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-    //    {
-    //        if (value is bool isChecked && isChecked && targetTypes.Length == 2)
-    //        {
-    //            return new object[] { targetTypes[0], null }; // Возвращаем текущую модель для обновления
-    //        }
-    //        return new object[] { Binding.DoNothing, Binding.DoNothing };
-    //    }
-    //}
-
     public class SelectedVariantConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
