9817c0d8ea13b801b724c81fe91bc8d7782250a9
Timur Kurskiev <kurskievtr@gmail.com>
2025-02-22 22:17:43 +0300

    доработки ГУИ

---
+++ UserSecretsManager/Converters/BooleanToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System;
+using System.Windows.Media;
+
+namespace UserSecretsManager.Converters;
+
+public class BooleanToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isSelected && isSelected)
+        {
+            return Brushes.Black; // Активный вариант — черный
+        }
+        return Brushes.Gray; // Неактивный — серый
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
